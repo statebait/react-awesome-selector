0c88501d9897bc9960d122e583ef37dfe869c373
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-08-15 01:26:00 +0530

    refactor: useContext hook usage

--- src/SelectList.jsx
+++ src/SelectList.jsx
@@ -4,37 +4,28 @@ import Item from './Item.jsx'
 import { DataContext } from './DataContext.jsx'
 
 function SelectList() {
+  const context = React.useContext(DataContext)
   return (
-    <DataContext.Consumer>
-      {(context) => {
+    <div>
+      {context.categories.map((category, categoryIndex) => {
         return (
-          <div>
-            {context.categories.map((category, categoryIndex) => {
-              return (
-                <Category key={categoryIndex} title={category}>
-                  {context.selectList.map((item) => {
-                    if (category === item.category) {
-                      return (
-                        <Item
-                          key={item.key}
-                          onIconClick={() => {
-                            context.addSelected(item)
-                          }}
-                        >
-                          {item.name}
-                        </Item>
-                      )
-                    } else {
-                      return null
-                    }
-                  })}
-                </Category>
-              )
+          <Category key={categoryIndex} title={category}>
+            {context.selectList.map((item) => {
+              if (category === item.category) {
+                const handleIconClick = () => context.addSelected(item)
+                return (
+                  <Item key={item.key} onIconClick={handleIconClick}>
+                    {item.name}
+                  </Item>
+                )
+              } else {
+                return null
+              }
             })}
-          </div>
+          </Category>
         )
-      }}
-    </DataContext.Consumer>
+      })}
+    </div>
   )
 }
 
--- src/SelectedList.jsx
+++ src/SelectedList.jsx
@@ -3,29 +3,22 @@ import Item from './Item.jsx'
 import { DataContext } from './DataContext.jsx'
 
 const SelectedList = (props) => {
+  const context = React.useContext(DataContext)
+
   return (
-    <DataContext.Consumer>
-      {(context) => (
-        <div className="react-awesome-selector-selected-list">
-          <span className="react-awesome-selector-selected-list-title">
-            {props.title}
-          </span>
-          {context.selectedList.map((item) => {
-            return (
-              <Item
-                key={item.key}
-                selected
-                onIconClick={() => {
-                  context.removeSelected(item)
-                }}
-              >
-                {item.name}
-              </Item>
-            )
-          })}
-        </div>
-      )}
-    </DataContext.Consumer>
+    <div className="react-awesome-selector-selected-list">
+      <span className="react-awesome-selector-selected-list-title">
+        {props.title}
+      </span>
+      {context.selectedList.map((item) => {
+        const handleIconClick = () => context.removeSelected(item)
+        return (
+          <Item key={item.key} selected onIconClick={handleIconClick}>
+            {item.name}
+          </Item>
+        )
+      })}
+    </div>
   )
 }
 
